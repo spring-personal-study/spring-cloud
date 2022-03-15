33c490b628a2eebfdeeb9d0236cfcd4703a5f16d
chaenabi <find1086@gmail.com>
2022-03-15 18:48:29 +0900

    set setting

--- src/main/java/cloud/ecommerce/EcommerceApplication.java
+++ src/main/java/cloud/ecommerce/EcommerceApplication.java
@@ -2,8 +2,10 @@ package cloud.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EcommerceApplication {
 
     public static void main(String[] args) {
